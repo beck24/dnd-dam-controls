d6575d00f18cc7d682d940e92cd61e82dcc1d347
Matt Beckett <beck24@gmail.com>
2024-08-17 20:57:25 -0700

    wip

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {},
+    fontFamily: {
+      retro: ["'Jersey 20'", 'sans-serif'],
+    },
+  },
+  plugins: [],
+}
+
